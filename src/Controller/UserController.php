b8cb812d9ae8855e85b264f31f5ca20fad466f60
Lord-Einstein <darylleinstein@gmail.com>
2026-01-11 04:25:48 +0100

    Feat : Add User showing Controller and implement the show.html.twig structure.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted("ROLE_USER")]
+final class UserController extends AbstractController
+{
+    #[Route('/user/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}

8565e6bd136445c0e688157ff120bb16becd6e33
Vresca <ilay-selin.kocak@coda-student.school>
2026-01-18 19:27:52 +0100

    has added fil d'actualité : TwwetRepository

--- src/Repository/TweetRepository.php
+++ src/Repository/TweetRepository.php
@@ -6,49 +6,22 @@ use App\Entity\Tweet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Tweet>
- */
 class TweetRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Tweet::class);
     }
+    public function findLatestTweet(): array
+    {
 
-//    /**
-//     * @return Tweet[] Returns an array of Tweet objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->ge  /**
-//     * @return Tweet[] Returns an array of Tweet objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+        return $this->createQueryBuilder('t')
+            ->innerJoin('t.author', 'u')
+            ->addSelect('u')
+            ->orderBy('t.createdAt', 'DESC')
+            ->setMaxResults(20)
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Tweet
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }

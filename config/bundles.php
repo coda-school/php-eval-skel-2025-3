77792a56ddc488739bd81a2591834f0cfd007bd8
Adam Valsan <adam.valsan3@gmail.com>
2026-02-06 00:29:13 +0100

    Fix: UX/UI, Lang, Feat: Pagination, Date format

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
